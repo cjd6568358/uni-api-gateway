5e0555dd81285c719851622d6c2bdf1d66afb3ac
jie.cao <jie.cao@tigerobo.com>
2021-08-31 10:11:27 +0800

    fix

--- utils/index.js
+++ utils/index.js
@@ -1,18 +1,3 @@
-let getProcessEnv = (key) => {
-  let value = null;
-  if (process.env[key]) {
-    value = process.env[key];
-  } else {
-    let argvs = process.argv.slice(2);
-    argvs.forEach((argv) => {
-      if (argv.startsWith(key + "=")) {
-        value = argv.split("=")[1];
-      }
-    });
-  }
-  return value;
-};
-
 const checkValid = (result) => {
   if (!result) {
     return false;
